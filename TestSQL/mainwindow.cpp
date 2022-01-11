4b18ea9b666f00608c8e048b7f1f3318986c8c56
Nick <yakimov_87@mail.ru>
2022-01-12 01:27:09 +0300

    first

---
+++ TestSQL/mainwindow.cpp
@@ -0,0 +1,112 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QMessageBox"
+#include <QSqlTableModel>
+#include <QTableView>
+#include <QSqlRecord>
+
+void MainWindow::connectionDB()
+{
+    m_db = QSqlDatabase::addDatabase("QSQLITE");
+    m_db.setDatabaseName("errors");
+    if (!m_db.open()) {
+        QMessageBox::critical(nullptr, QObject::tr("Cannot open database"),
+            QObject::tr("Unable to establish a database connection.\n"
+                        "This example needs SQLite support. Please read "
+                        "the Qt SQL driver documentation for information how "
+                        "to build it.\n\n"
+                        "Click Cancel to exit."), QMessageBox::Cancel);
+
+    } else {
+        qDebug() << "m_db is open";
+    }
+
+}
+
+void MainWindow::createTables()
+{
+    qDebug() << "create Table";
+    QSqlQuery query;
+    query.exec("CREATE TABLE E1 (id INT PRIMARY KEY, comments VARCHAR(100))");
+    query.exec("INSERT INTO E1 (id,comments) VALUES (1,'E1 First comment'),(2, 'E1 Second Comments'),(3, 'E1 Three comments')");
+
+    query.exec("CREATE TABLE E2 (id INT PRIMARY KEY, comments VARCHAR(100))");
+    query.exec("INSERT INTO E2 (id, comments) VALUES (1, 'E2 First comment'),(2, 'E2 Second Comments'),(3, 'E2 Three comments')");
+
+    query.exec("CREATE TABLE E3 (id INT PRIMARY KEY, comments VARCHAR(100))");
+    query.exec("INSERT INTO E3 (id, comments) VALUES (1, 'E3 First comment'),(2, 'E3 Second Comments'),(3, 'E3 Three comments')");
+
+
+}
+
+void MainWindow::addTable()
+{
+    qDebug() << "add Tables";
+    m_listTable = m_db.tables();
+    qDebug() << m_listTable.count();
+    foreach (QString table, m_listTable) {
+        qDebug() << table;
+
+    }
+    ui->comboBox_dev->addItems(m_listTable);
+}
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+
+//    ui->comboBox_dev->addItem("E1");
+//    ui->comboBox_dev->addItem("E2");
+//    ui->comboBox_dev->addItem("E3");
+
+    connectionDB();
+    createTables();
+    addTable();
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+//void MainWindow::on_comboBox_dev_currentIndexChanged(int index)
+//{
+//    switch (index) {
+//    case 0:
+
+//        break;
+//    default:
+//        break;
+//    }
+//}
+
+void MainWindow::on_comboBox_dev_currentIndexChanged(const QString &arg1)
+{
+    qDebug() << arg1;
+    QSqlTableModel model;
+    model.setTable(arg1);
+    model.select();
+    model.setEditStrategy(QSqlTableModel::OnManualSubmit);
+
+    qDebug() << model.rowCount();
+
+    for (int nRow = 0; nRow < model.rowCount(); ++nRow) {
+    QSqlRecord rec = model.record(nRow);
+    int nNumber = rec.value("id").toInt();
+    qDebug() << nNumber;
+    QString strName = rec.value ( "comments").toString();
+    qDebug() << strName;
+    }
+    //model.setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
+    //model.setHeaderData(1, Qt::Horizontal, QObject::tr("Comments"));
+
+    ui->tableView_comments->setModel(&model);
+
+    ui->tableView_comments->show();
+}
---
+++ TestSQL/mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSQLDatabase>
+#include <QStringList>
+#include <QDebug>
+#include <QSqlQuery>
+#include <QSqlTableModel>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    void connectionDB();
+    void createTables();
+    void addTable();
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    //void on_comboBox_dev_currentIndexChanged(int index);
+
+    void on_comboBox_dev_currentIndexChanged(const QString &arg1);
+
+private:
+    Ui::MainWindow *ui;
+    QSqlDatabase m_db;
+    QStringList m_listTable;
+
+};
+#endif // MAINWINDOW_H
